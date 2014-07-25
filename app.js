0b9accf1a0a851f5901993302874c95003c722d3
Jesse Lilly <jesselilly@gmail.com>
2014-07-25 05:22:44 +0000

    Removed c9

--- app.js
+++ app.js
@@ -4,15 +4,41 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sql = require('mssql'); 
 
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('ct:2e79401dbfb841a29094b3790e62388b@kahana.mongohq.com:10025/nodetest');
+// var mongo = require('mongodb');
+// var monk = require('monk');
+// var db = monk('ct:2e79401dbfb841a29094b3790e62388b@kahana.mongohq.com:10025/nodetest');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// var config = {
+//     Server=tcp:qqy8bap74c.database.windows.net,
+//     1433;Database=Learn;Uid=cttest@qqy8bap74c;Pwd={your_password_here};Encrypt=yes;Connection Timeout=30;
+// }
+
+
+var config = {
+    user: 'cttest@qqy8bap74c',
+    password: '*Tank.8280',
+    server: 'tcp:qqy8bap74c.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+    port: '1433',
+    database: 'Learn',
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }}
+
+sql.connect(config, function(err) {
+    var request = new sql.Request();
+    request.query('select * from Users', function(err, recordset) {
+        // ... error checks
+
+        console.dir(recordset);
+    });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,10 +51,10 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
+// app.use(function(req,res,next){
+//     req.db = db;
+//     next();
+// });
 
 app.use('/', routes);
 app.use('/users', users);
